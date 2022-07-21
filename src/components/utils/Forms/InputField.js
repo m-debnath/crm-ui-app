c4a179f0bdc1fa9a0dbc91aa3f24e7615e0d82c2
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-21 13:03:12 +0530

    changed input hover border

--- src/components/utils/Forms/InputField.js
+++ src/components/utils/Forms/InputField.js
@@ -1,8 +1,9 @@
 const InputField = (props) => {
   return (
     <input
+      ref={props.local_ref}
       required={props.required}
-      className="w-48 t-2 mr-2 p-2 rounded-md border-[1px] shadow-inner hover:border-black hover:border-2"
+      className="w-48 t-2 mr-2 p-2 rounded border-[1px] shadow-inner hover:border-slate-600 hover:border-[1px]"
       type={props.type}
       {...props}
     />
