e86a35c9a28be1b73f56f270e0aad4ab2ad79c88
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-22 23:19:48 +0530

    Error Message Position changed.

--- src/components/utils/Notifications/ErrorNotify.js
+++ src/components/utils/Notifications/ErrorNotify.js
@@ -6,8 +6,8 @@ const ErrorNotify = (props) => {
       <p
         className={
           props.errorMessage
-            ? "transform transition-all text-red-600 text-sm text-bold text-center h-7 pt-2"
-            : "transform transition-all text-transparent text-sm text-bold text-center h-7 pt-2"
+            ? "transform transition-all text-red-600 text-sm text-bold text-center h-7"
+            : "transform transition-all text-transparent text-sm text-bold text-center h-7"
         }
       >
         {props.errorMessage}
