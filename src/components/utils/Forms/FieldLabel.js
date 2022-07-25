c73c02be33f383c2c32f25ff22f6b847fc0c2538
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-26 03:14:26 +0530

    Added flowbite

--- src/components/utils/Forms/FieldLabel.js
+++
@@ -1,9 +0,0 @@
-const FieldLabel = (props) => {
-  return (
-    <label className="ml-2 t-2 my-[auto]" htmlFor={props.htmlFor} {...props}>
-      {props.children}
-    </label>
-  );
-};
-
-export default FieldLabel;
